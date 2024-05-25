84496c2160a6554dced8eacd2f3497793245d16e
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-25 23:19:43 +0600

    Create common types

---
+++ src/types/common-types.ts
@@ -0,0 +1,14 @@
+export type TBloodTypes = "A_POSITIVE" |
+  "A_NEGATIVE" |
+  "B_POSITIVE" |
+  "B_NEGATIVE" |
+  "AB_POSITIVE" |
+  "AB_NEGATIVE" |
+  "O_POSITIVE" |
+  "O_NEGATIVE";
+
+export type TUserRole = "SUPER_ADMIN" | "ADMIN" | "USER";
+
+export type TUserStatus = "ACTIVE" | "INACTIVE" | "BLOCKED";
+
+export type TGender = "MALE" | "FEMALE";
