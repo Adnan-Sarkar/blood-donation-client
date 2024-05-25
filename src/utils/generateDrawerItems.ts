f7060cb896e76aa4e3cbc110a5e7a337b527aa18
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-26 02:21:41 +0600

    Create utility function to generate role based dashboard drawer items

---
+++ src/utils/generateDrawerItems.ts
@@ -0,0 +1,100 @@
+import { TDrawerItem, TUserRole } from "@/types";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
+import KeyRoundedIcon from "@mui/icons-material/KeyRounded";
+import BloodtypeRoundedIcon from "@mui/icons-material/BloodtypeRounded";
+
+export const generateDrawerItems = (role: TUserRole): TDrawerItem[] => {
+  const roleMenus: TDrawerItem[] = [];
+
+  const defaultMenus = [
+    {
+      title: "Change Password",
+      path: `change-password`,
+      icon: KeyRoundedIcon,
+    },
+  ];
+
+  switch (role) {
+    case "ADMIN":
+      roleMenus.push(
+        {
+          title: "Dashboard",
+          path: `${role.toLowerCase()}`,
+          icon: DashboardOutlinedIcon,
+        },
+        {
+          title: "Profile",
+          path: `${role.toLowerCase()}/profile`,
+          icon: AccountBoxIcon,
+        },
+        {
+          title: "Manage Users",
+          path: `${role.toLowerCase()}/manage-users`,
+          icon: AccountBoxIcon,
+        },
+        {
+          title: "Manage Donations",
+          path: `${role.toLowerCase()}/manage-donations`,
+          icon: BloodtypeRoundedIcon,
+        }
+      );
+      break;
+    case "SUPER_ADMIN":
+      roleMenus.push(
+        {
+          title: "Dashboard",
+          path: `${role.toLowerCase()}`,
+          icon: DashboardOutlinedIcon,
+        },
+        {
+          title: "Profile",
+          path: `${role.toLowerCase()}/profile`,
+          icon: AccountBoxIcon,
+        },
+        {
+          title: "Manage Users",
+          path: `${role.toLowerCase()}/manage-users`,
+          icon: AccountBoxIcon,
+        },
+        {
+          title: "Manage Donations",
+          path: `${role.toLowerCase()}/manage-donations`,
+          icon: BloodtypeRoundedIcon,
+        }
+      );
+      break;
+    case "USER":
+      roleMenus.push(
+        {
+          title: "Dashboard",
+          path: `${role.toLowerCase()}`,
+          icon: DashboardOutlinedIcon,
+        },
+        {
+          title: "Profile",
+          path: `${role.toLowerCase()}/profile`,
+          icon: AccountBoxIcon,
+        },
+        {
+          title: "Sent Blood Requests",
+          path: `${role.toLowerCase()}/sent-requests`,
+          icon: SendRoundedIcon,
+        },
+        {
+          title: "Received Blood Requests",
+          path: `${role.toLowerCase()}/received-requests`,
+          icon: MessageOutlinedIcon,
+        },
+      );
+      break;
+    default:
+      break;
+  }
+
+  roleMenus.push(...defaultMenus)
+
+  return roleMenus
+}
