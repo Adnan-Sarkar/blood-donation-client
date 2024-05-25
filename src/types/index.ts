228681a79abb4697050c5ebeb564da71070a3585
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-25 23:19:18 +0600

    Create user types and update index file of types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 export * from "./response-types";
 export * from "./error-types";
 export * from "./componentProps-types";
+export * from "./user-types";
+export * from "./common-types";
---
+++ src/types/user-types.ts
@@ -0,0 +1,24 @@
+import { TBloodTypes, TGender, TUserRole, TUserStatus } from "@/types/common-types";
+
+export type TUserProfile = {
+  id: string;
+  userId: string;
+  bio: string;
+  age: number;
+  lastDonationDate: string;
+};
+
+export type TUser = {
+  id:                  string;
+  name:                string;
+  email:               string;
+  password:            string;
+  gender:              TGender;
+  bloodType:           TBloodTypes;
+  role:                TUserRole;
+  location:            string
+  profilePicture:      string;
+  availability:        boolean;
+  userProfile:         TUserProfile;
+  status: TUserStatus;
+};
