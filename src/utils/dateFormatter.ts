6a1b41198d83775aaeab04fbad0172d87a92ce4d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-29 00:06:58 +0600

    Create utility function to format date from date picker

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,11 @@
+const dateFormatter = (value: string) => {
+  const date = new Date(value);
+
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return `${day}-${month}-${year}`;
+};
+
+export default dateFormatter;
