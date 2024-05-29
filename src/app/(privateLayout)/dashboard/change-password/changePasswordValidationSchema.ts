1f1bfe288e9e6ebc179252fcfebe691e1f35b96f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-29 09:04:46 +0600

    Create validation schema for change password using zod

--- src/app/(privateLayout)/dashboard/change-password/changePasswordValidationSchema.ts
+++ src/app/(privateLayout)/dashboard/change-password/changePasswordValidationSchema.ts
@@ -4,13 +4,13 @@ export const changePasswordValidationSchema = z.object({
   oldPassword: z.string({
     required_error: "Old Password is required",
     invalid_type_error: "Old Password must be string",
-  }),
+  }).min(5, "Old Password must be minimum 5 characters long"),
   newPassword: z.string({
     required_error: "New Password is required",
     invalid_type_error: "New Password must be string",
-  }),
+  }).min(5, "New Password must be minimum 5 characters long"),
   confirmPassword: z.string({
     required_error: "Confirm Password is required",
     invalid_type_error: "Confirm Password must be string",
-  }),
+  }).min(5, "Confirm Password must be minimum 5 characters long"),
 });
