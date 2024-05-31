56a4f4c0e39f728bc3c326d816c18db904777d5b
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-31 16:51:40 +0600

    Add new tag-types

--- src/redux/tag-types.ts
+++ src/redux/tag-types.ts
@@ -2,10 +2,12 @@ export enum tagTypes {
   superAdmin = "superAdmin",
   admin = "admin",
   user = "user",
+  donors = "donors",
 }
 
 export const tagTypesList = [
   tagTypes.superAdmin,
   tagTypes.admin,
   tagTypes.user,
+  tagTypes.donors
 ];
