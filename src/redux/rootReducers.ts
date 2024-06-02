bade0e042e5e564d5c92ef228a98c14f80b8e87e
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-02 23:59:14 +0600

    Update root reducer

--- src/redux/rootReducers.ts
+++ src/redux/rootReducers.ts
@@ -1,5 +1,20 @@
 import baseApi from "@/redux/api/baseApi";
+import userReducer from "@/redux/features/user/userSlice";
+import tokenReducer from "@/redux/features/user/tokenSlice";
+// @ts-ignore
+import storage from "redux-persist/lib/storage";
+import { authKey } from "@/constant/authKey";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: authKey,
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, tokenReducer);
 
 export const reducer = {
-  [baseApi.reducerPath]: baseApi.reducer
+  [baseApi.reducerPath]: baseApi.reducer,
+  user: userReducer,
+  token: persistedAuthReducer,
 }
