9ee7748ab8700dddb8fc0cc35a0939ebaec3a607
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-02 23:57:59 +0600

    update redux store with redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
-import {reducer} from "./rootReducers";
+import { configureStore } from "@reduxjs/toolkit";
+import { reducer } from "./rootReducers";
 import baseApi from "@/redux/api/baseApi";
+import { FLUSH, PAUSE, PERSIST, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import { Persistor } from "redux-persist/es/types";
+
 
 export const store = configureStore({
     reducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(baseApi.middleware),
+      getDefaultMiddleware({
+          serializableCheck: {
+              ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+      }).concat(baseApi.middleware),
 });
 
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor: Persistor = persistStore(store);
