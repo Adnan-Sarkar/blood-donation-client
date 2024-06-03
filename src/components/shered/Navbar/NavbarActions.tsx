96ea58a742488a2f50e1235bfec1e0931207ef60
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-03 20:42:35 +0600

    Update Navbar actions using redux

--- src/components/shered/Navbar/NavbarActions.tsx
+++ src/components/shered/Navbar/NavbarActions.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { getUserInfo } from "@/services/auth.services";
+"use client";
+
+import React, { useEffect } from "react";
 import ProfileAvatar from "@/components/shered/ProfileAvatar/ProfileAvatar";
 import Link from "next/link";
 import { Button } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { getUserInfoFromLocalStorage } from "@/redux/features/user/userSlice";
 
 const NavbarActions = () => {
-  const userInfo = getUserInfo();
+  const [userInfo, setUserInfo] = React.useState<any>(null);
+  const dispatch = useAppDispatch();
+  const { user, token } = useAppSelector((state) => state);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getUserInfoFromLocalStorage());
+      if (user?.id) {
+        setUserInfo(user);
+      }
+      else {
+        setUserInfo(null);
+      }
+    }
+  }, [user, token, dispatch]);
 
   return (
     <>
     {
-      (userInfo && userInfo.id) ? <ProfileAvatar /> : <Button><Link href={"/login"}>Login</Link></Button>
+      (userInfo && userInfo?.token) ? <ProfileAvatar /> : <Button><Link href={"/login"}>Login</Link></Button>
     }
     </>
   );
