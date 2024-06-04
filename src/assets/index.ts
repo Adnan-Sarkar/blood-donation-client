3f1987dd34494dbad0ba435dd8e8cf0daf94bc49
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-04 09:45:54 +0600

    Add new images and update assets index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,11 @@ const assets = {
   images: {
     logo: require("./images/logo.png"),
     avatar: require("./images/default-avatar.png"),
+    logoIcon: require("./images/logo-icon.png"),
+    facebook: require("./images/facebook.png"),
+    instagram: require("./images/instagram.png"),
+    x: require("./images/x.png"),
+    threads: require("./images/threads.png.png"),
   }
 }
 
