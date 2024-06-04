2ea6ef699ad5891ada4a7b8f21f7dc5d7a6ac3a7
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-04 21:30:37 +0600

    Add new images and update assets index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,23 @@
 const assets = {
   images: {
     logo: require("./images/logo.png"),
+    logoWhite: require("./images/logo-white.png"),
     avatar: require("./images/default-avatar.png"),
     logoIcon: require("./images/logo-icon.png"),
     facebook: require("./images/facebook.png"),
     instagram: require("./images/instagram.png"),
     x: require("./images/x.png"),
-    threads: require("./images/threads.png.png"),
+    threads: require("./images/threads.png"),
+    requestToDonate: require("./images/request-donation.png"),
+    donateBlood: require("./images/donate-blood.png"),
+    bloodBank: require("./images/blood-bank.png"),
+    donationTip1: require("./images/donation-tip-1.png"),
+    donationTip2: require("./images/donation-tip-2.png"),
+    donationTip3: require("./images/donation-tip-3.png"),
+    donationTip4: require("./images/donation-tip-4.png"),
+    donationTip5: require("./images/donation-tip-5.png"),
+    donationTip6: require("./images/donation-tip-6.png"),
+    donationTip7: require("./images/donation-tip-7.png"),
   }
 }
 
