972dfe67ed40db2e1f43cab0132b8a71a94dc05c
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-05 22:41:48 +0600

    Update donors page

--- src/app/(publicLayout)/donors/page.tsx
+++ src/app/(publicLayout)/donors/page.tsx
@@ -5,16 +5,48 @@ import { Box, Container } from "@mui/material";
 import DonorsSearch from "@/app/(publicLayout)/donors/components/DonorsSearch";
 import DonorLists from "@/app/(publicLayout)/donors/components/DonorLists";
 import { useGetAllDonorsQuery } from "@/redux/api/donorApi";
+import { useAppSelector } from "@/redux/hooks";
+import { TUser } from "@/types";
+import { getUserInfo } from "@/services/auth.services";
 
 const DonorsPage = () => {
-  const [searchContent, setSearchContent] = React.useState<string>("");
-  const {data} = useGetAllDonorsQuery({});
+  const userInfo = getUserInfo();
+  const searchDonors = useAppSelector((state) => state.search.searchDonors);
+  const filterDonors = useAppSelector((state) => state.filter.filterDonors);
+
+  let filterObj = {};
+  if (filterDonors.bloodType && filterDonors.availability) {
+    filterObj = {
+      bloodType: filterDonors.bloodType,
+      availability: filterDonors.availability === "Available"
+    }
+  }
+  else if (filterDonors.availability) {
+    filterObj = {
+      availability: filterDonors.availability === "Available"
+    }
+  }
+  else if (filterDonors.bloodType) {
+    filterObj = {
+      bloodType: filterDonors.bloodType
+    }
+  }
+
+  console.log({ filterDonors });
+
+  const {data} = useGetAllDonorsQuery({
+    searchTerm: searchDonors || "",
+    ...filterObj
+  });
+
 
   return (
     <Container>
-      <DonorsSearch setContent={setSearchContent} />
-      <Box my={12}></Box>
-      <DonorLists donorList={data} />
+      <Box mb={6}>
+        <DonorsSearch />
+        <Box my={12}></Box>
+        <DonorLists donorList={data?.filter((donor: TUser) => (donor.id !== userInfo?.id))} />
+      </Box>
     </Container>
   );
 };
