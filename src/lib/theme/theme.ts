ebdb87e52f5f4e922d99644e3d9da5470e8c37e0
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-08 21:32:49 +0600

    Update theme

--- src/lib/theme/theme.ts
+++ src/lib/theme/theme.ts
@@ -1,13 +1,14 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
     palette: {
         primary: {
-            main: "#fc5c65",
-        },
-        secondary: {
             main: "#34495e",
         },
+        secondary: {
+            main: "#fc5c65",
+        },
+
     },
     components: {
         MuiButton: {
@@ -19,8 +20,28 @@ const theme = createTheme({
             defaultProps: {
                 maxWidth: "lg",
             },
-        }
-    }
+        },
+    },
+    typography: {
+        fontFamily: [
+            "Poppins",
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(","),
+        fontSize: 15,
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        fontWeightBold: 700,
+    },
 });
 
 export default theme;
